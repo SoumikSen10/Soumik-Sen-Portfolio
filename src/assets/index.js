15049953c7fb499c2527352ee1f990607be6ff99
Soumik Sen <mastersoumik2003@gmail.com>
2025-01-22 22:23:52 +0530

    Adding final changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
 import PredictiX from './SS1.png';
 import VoxDebate from './Playground.png';
 import CryptoSphere from './CryptoSphere.jpeg';
+import Logo1 from "./Logo1.png";
 
-export { PredictiX,VoxDebate, CryptoSphere };
+export { PredictiX,VoxDebate, CryptoSphere,Logo1 };
--- src/components/SidebarDemo.jsx
+++ src/components/SidebarDemo.jsx
@@ -16,6 +16,8 @@ import {
 import { Link } from "react-router-dom"; // Replace with "react-router-dom" for Vite
 import { motion } from "framer-motion";
 
+import {Logo1} from "../assets/index";
+
 import cn from "classnames"; // Ensure classnames library is installed
 
 
@@ -87,22 +89,27 @@ export const Logo = () => (
     to="/"
     className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
   >
-    <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+    
+    <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" >
+    <img src={Logo1} alt="logo" className="h-5 w-5" />
+    </div>
     <motion.span
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       className="font-medium text-black dark:text-white whitespace-pre"
     >
-      Portfolio
+      OUMIK
     </motion.span>
   </Link>
 );
 
 export const LogoIcon = () => (
   <Link
-    to="#"
+    to="/"
     className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
   >
-    <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+    <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0">
+      <img src={Logo1} alt="logo" className="h-5 w-5" />
+    </div>
   </Link>
 );
